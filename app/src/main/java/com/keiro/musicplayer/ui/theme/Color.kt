624bd376c6bfabb3c6486c32acbfc2fb940a5d4b
KeiRo-Developer <benningtonchester438@gmail.com>
2026-08-03 20:42:38 +0800

    Create Color.kt

---
+++ app/src/main/java/com/keiro/musicplayer/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.keiro.musicplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AccentViolet = Color(0xFFB388FF)
+val AccentCyan = Color(0xFF64FFDA)
+val DeepBlack = Color(0xFF0A0A0D)
+val SurfaceDark = Color(0xFF16151C)
+val SurfaceElevated = Color(0xFF201F29)
+val OnSurfaceMuted = Color(0xFF9C9AA8)
+val LosslessGold = Color(0xFFE9C46A)
